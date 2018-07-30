2b0108da617f04f609e8368a37341903001fcaef
macq@MacQs-MacBook.(none) <macq@MacQs-MacBook.local>
2018-07-30 15:16:35 +0700

    Add Rol

--- app/scripts/nodes.js
+++ app/scripts/nodes.js
@@ -29,8 +29,8 @@ nodes.nodeList = {
         'type': nodes.nodeTypes.ROLLER,
         'eip155': true,
         'chainId': 29021982,
-        'tokenList': require('./tokens/akromaTokens.json'),
-        'abiList': require('./abiDefinitions/akromaAbi.json'),
+        'tokenList': require('./tokens/rolTokens.json'),
+        'abiList': require('./abiDefinitions/rolAbi.json'),
         'service': 'Roller Offical',
         'lib': new nodes.customNode('https://node.roller.today', '')
     }

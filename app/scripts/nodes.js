c0fba3f23c0d521123cc1cfd2ba6d9f13d6c296f
macq@MacQs-MacBook.(none) <macq@MacQs-MacBook.local>
2018-07-30 09:34:52 +0700

    Add ROL

--- app/scripts/nodes.js
+++ app/scripts/nodes.js
@@ -4,21 +4,21 @@ nodes.customNode = require('./nodeHelpers/customNode');
 nodes.infuraNode = require('./nodeHelpers/infura');
 nodes.metamaskNode = require('./nodeHelpers/metamask');
 nodes.nodeTypes = {
-    ETH: "PIRL"
+    ETH: "ROL"
 };
 nodes.ensNodeTypes = [nodes.nodeTypes.ETH, nodes.nodeTypes.Ropsten];
 nodes.nodeList = {
     'eth_mew': {
-        'name': 'PIRL',
+        'name': 'ROL',
         'blockExplorerTX': 'https://poseidon.pirl.io/explorer/transaction/[[txHash]]',
         'blockExplorerAddr': 'https://poseidon.pirl.io/explorer/address/[[address]]',
         'type': nodes.nodeTypes.ETH,
         'eip155': true,
-        'chainId': 3125659152,
+        'chainId': 29021982,
         'tokenList': require('./tokens/ethTokens.json'),
         'abiList': require('./abiDefinitions/ethAbi.json'),
         'service': 'MyPirlWallet',
-        'lib': new nodes.customNode('https://wallrpc.pirl.io', '')
+        'lib': new nodes.customNode('http://node.roller.today', '')
     }
 };
 
